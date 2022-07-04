b1def1be20c1ee4e963fc4d4c4cc0656bae004f2
lilbillybiscuit <qianbill2014@outlook.com>
2022-07-04 15:34:28 -0400

    Adapt to react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import {
   BrowserRouter,
   Routes,
   Route,
-  Switch,
-  useRouteMatch,
-  useParams,
 } from "react-router-dom";
 import GradAppBar from "./components/GradAppBar";
 
@@ -15,24 +12,6 @@ import ActivityPage from "./pages/ActivityPage";
 import HomePage from "./pages/HomePage";
 import GuestsPage from "./pages/GuestsPage";
 import MusicControl from "./pages/MusicControl";
-
-import Slide from "@mui/material/Slide";
-import useScrollTrigger from "@mui/material/useScrollTrigger";
-function HideOnScroll(props) {
-  const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
-  });
-
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  );
-}
 
 function App() {
   return (
